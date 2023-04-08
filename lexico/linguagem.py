91f95a03be73b38e6a5ab00531c8fac448d1fbd0
Luiz Felipe Teodoro Monteiro <luiz.felipe.monteiro21@gmail.com>
2023-04-08 16:48:46 -0300

    trabalho compiladores

---
+++ lexico/linguagem.py
@@ -0,0 +1,41 @@
+OPERADORES = {
+    '==': '==',
+    '<>':'<>',
+    '>=':'>=',
+    '<=':'<=',
+    '>':'>',
+    '<':'<',
+    '=': '=',
+    '+':'+',
+    '-':'-',
+    '*':'*',
+    '/':'/'
+}
+
+LALG = {
+    'program': 'program',
+    'begin': 'begin',
+    'end': 'end',
+    'var': 'var',
+    'real': 'real',
+    'integer': 'integer',
+    'procedure':'procedure',
+    'read': 'read',
+    'write': 'write',
+    'if':'if',
+    'then':'then',
+    'else': 'else',
+    'repeat': 'repeat',
+    'until':'until',
+    'while': 'while',
+    'do': 'do'
+}
+
+PONTOS = {
+    '.':'.',
+    ',':',',
+    '(':'(',
+    ')':')',
+    ':':':',
+    ';':';'
+}
